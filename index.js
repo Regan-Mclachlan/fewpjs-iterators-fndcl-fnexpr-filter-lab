6121f18f4f4b85124a8511a2a9f31410dc1c7131
Regan-Mclachlan <reganmc96@hotmail.com>
2021-03-10 15:46:27 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter( match =>
+      match.toLowerCase() === string.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(drivers, string) {
+    return drivers.filter( letters =>
+      letters.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(drivers, string) {
+    return drivers.filter( details => details.name === string)
+  }
